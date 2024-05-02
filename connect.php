c4ec359bfbb6033eaf2b5c982d8acb5dbe530a00
KURIA FELIX MUHIA <160228937+Kuria-FelixM@users.noreply.github.com>
2024-05-02 15:12:28 +0300

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "e-commerce";
+
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+
+
+?>
